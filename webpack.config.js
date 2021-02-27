3e9ee3404800fc0d7b2891c4929823a347a9521a
Johnny Almonte <johnny243@users.noreply.github.com>
2021-02-27 02:30:34 -0400

    fix: lint

--- webpack.config.js
+++ webpack.config.js
@@ -34,7 +34,8 @@ module.exports = (_, { mode }) => ({
     headers: {
       'Access-Control-Allow-Origin': '*',
       'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
-      'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization',
+      'Access-Control-Allow-Headers':
+        'X-Requested-With, content-type, Authorization',
     },
   },
   module: {
