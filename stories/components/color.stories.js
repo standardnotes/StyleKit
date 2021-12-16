7c2de7caa017bd72e9d35c0d853bddce7a799939
Laurent Senta <laurent@singulargarden.com>
2021-12-16 10:51:03 +0100

    fix: fix select & args

--- stories/components/color.stories.js
+++ stories/components/color.stories.js
@@ -1,36 +1,35 @@
 import { h } from 'preact';
-import { Button } from '@Components/Button';
 
 export default {
   title: 'Design System/Styles/Colors',
-  component: Button,
   argTypes: {
     onClick: {
-      action: 'clicked'
+      action: 'clicked',
     },
     variant: {
       control: {
         type: 'select',
         options: [
-          "black",
-          "grey-1",
-          "grey-2",
-          "grey-3",
-          "grey-4",
-          "grey-5",
-          "grey-6",
-          "danger",
-          "success"
-        ]
-      }
-    }
+          'black',
+          'grey-1',
+          'grey-2',
+          'grey-3',
+          'grey-4',
+          'grey-5',
+          'grey-6',
+          'danger',
+        ],
+      },
+    },
   },
 };
 
-const Template = (args) => <label className={`color-${args.variant}`} >Helloworld</label>;
+const Template = (args) => (
+  <label className={`color-${args.variant}`}>{args.label}</label>
+);
 
 export const Info = Template.bind({});
 Info.args = {
-  label: 'Create note',
-  variant: 'info'
+  label: 'Some Label',
+  variant: 'info',
 };
