f3f81caa76531ae51ab23230423d42568e777786
Aman Harwara <amanharwara@protonmail.com>
2022-03-02 19:56:25 +0530

    feat: use classes instead of inline styles

--- src/components/Toast/ToastContainer.tsx
+++ src/components/Toast/ToastContainer.tsx
@@ -4,20 +4,11 @@ import { useStore } from '@nanostores/preact';
 import { toastStore } from './toastStore';
 import { Toast } from './Toast';
 
-const DEFAULT_OFFSET = '1.5rem';
-
 export const ToastContainer: FunctionComponent = () => {
   const toasts = useStore(toastStore);
 
   return (
-    <div
-      className="flex flex-col items-end fixed"
-      style={{
-        zIndex: 11000,
-        bottom: DEFAULT_OFFSET,
-        right: DEFAULT_OFFSET,
-      }}
-    >
+    <div className="flex flex-col items-end fixed z-index-toast bottom-6 right-6">
       {toasts.map((toast, index) => (
         <Toast toast={toast} index={index} key={toast.id} />
       ))}
