1f13aec70fa44f6e6142308f80e60f4c870bff71
Aman Harwara <amanharwara@protonmail.com>
2022-03-03 15:50:38 +0530

    feat: add new icons

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,23 +1,34 @@
 import AccessibilityIcon from './ic-accessibility.svg';
 import AccountCircleIcon from './ic-account-circle.svg';
 import AccountIllustration from './il-account.svg';
+import AddBoldIcon from './ic-add-bold.svg';
 import AddIcon from './ic-add.svg';
 import ArchiveIcon from './ic-archive.svg';
 import ArrowDownCheckmarkIcon from './arrow-down-checkmark.svg';
-import ArrowUpIcon from './ic-arrow-up.svg';
 import ArrowDownIcon from './ic-arrow-down.svg';
 import ArrowLeftIcon from './ic-arrow-left.svg';
 import ArrowRightIcon from './ic-arrow-right.svg';
+import ArrowsHorizontalIcon from './ic-arrows-horizontal.svg';
 import ArrowsSortDownIcon from './ic-arrows-sort-down.svg';
 import ArrowsSortUpIcon from './ic-arrows-sort-up.svg';
+import ArrowsVerticalIcon from './ic-arrows-vertical.svg';
+import ArrowUpIcon from './ic-arrow-up.svg';
+import AttachmentFileIcon from './ic-attachment-file.svg';
 import AuthenticatorIcon from './ic-authenticator.svg';
+import BackIosIcon from './ic-back-ios.svg';
 import BlueDotIcon from './blue-dot.svg';
+import BoxFilledIcon from './ic-box-filled.svg';
+import BoxIcon from './ic-box.svg';
+import CameraIcon from './ic-camera.svg';
+import CheckAllIcon from './ic-check-all.svg';
 import CheckBoldIcon from './ic-check-bold.svg';
-import CheckCircleIcon from './ic-check-circle.svg';
 import CheckCircleFilledIcon from './ic-check-circle-filled.svg';
+import CheckCircleIcon from './ic-check-circle.svg';
 import CheckIcon from './ic-check.svg';
 import ChevronDownIcon from './ic-chevron-down.svg';
+import ChevronLeftIcon from './ic-chevron-left.svg';
 import ChevronRightIcon from './ic-chevron-right.svg';
+import ChevronUpIcon from './ic-chevron-up.svg';
 import CircleIcon from './circle-55.svg';
 import ClearCircleFilledIcon from './ic-clear-circle-filled.svg';
 import CloseIcon from './ic-close.svg';
@@ -28,63 +39,89 @@ import CreateAccountIllustration from './create-account-illustration.svg';
 import DashboardIcon from './ic-dashboard.svg';
 import DiamondIcon from './diamond-with-horizontal-lines.svg';
 import DownloadIcon from './ic-download.svg';
+import DrawIcon from './ic-draw.svg';
 import EditorIcon from './ic-editor.svg';
 import EmailIcon from './ic-email.svg';
 import EyeIcon from './ic-eye.svg';
 import EyeOffIcon from './ic-eye-off.svg';
+import FileFilledIcon from './ic-file-filled.svg';
+import FileIcon from './ic-file.svg';
+import FolderFilledIcon from './ic-folder-filled.svg';
+import FolderIcon from './ic-folder.svg';
+import FolderKeyFilledIcon from './ic-folder-key-filled.svg';
+import ForwardIosIcon from './ic-forward-ios.svg';
 import HashtagIcon from './ic-hashtag.svg';
 import HashtagOffIcon from './ic-hashtag-off.svg';
+import HeartFilledIcon from './ic-heart-filled.svg';
 import HelpIcon from './ic-help.svg';
 import HistoryIcon from './ic-history.svg';
 import IlNotesIcon from './il-notes.svg';
+import ImageIcon from './ic-image.svg';
 import InfoIcon from './ic-info.svg';
 import KeyboardIcon from './ic-keyboard.svg';
+import LinkIcon from './ic-link.svg';
 import LinkOffIcon from './ic-link-off.svg';
 import ListBulleted from './ic-list-bulleted.svg';
-import ListedIcon from './ic-listed.svg';
 import ListedFilledIcon from './ic-listed-filled.svg';
+import ListedIcon from './ic-listed.svg';
 import LockFilledIcon from './ic-lock-filled.svg';
 import LockIcon from './ic-lock.svg';
 import MarkdownIcon from './ic-markdown.svg';
 import MenuArrowDownAlt from './ic-menu-arrow-down-alt.svg';
 import MenuArrowDownIcon from './ic-menu-arrow-down.svg';
-import MenuArrowRight from './ic-menu-arrow-right.svg';
+import MenuArrowRightIcon from './ic-menu-arrow-right.svg';
 import MenuCloseIcon from './ic-menu-close.svg';
+import MergeIcon from './ic-merge.svg';
 import MoreIcon from './ic-more.svg';
+import MoreVertIcon from './ic-more-vert.svg';
 import NotesIcon from './ic-notes.svg';
 import OpenInIcon from './ic-open-in.svg';
 import PasswordIcon from './ic-textbox-password.svg';
-import PencilOffIcon from './ic-pencil-off.svg';
-import PencilIcon from './ic-pencil.svg';
 import PencilFilledIcon from './ic-pencil-filled.svg';
+import PencilIcon from './ic-pencil.svg';
+import PencilOffIcon from './ic-pencil-off.svg';
 import PinFilledIcon from './ic-pin-filled.svg';
 import PinIcon from './ic-pin.svg';
 import PlainTextIcon from './ic-text-paragraph.svg';
+import PlusCircleFilledIcon from './ic-plus-circle-filled.svg';
+import PlusCircleIcon from './ic-plus-circle.svg';
 import PremiumFeatureIcon from './ic-premium-feature.svg';
 import PremiumIllustration from './il-premium.svg';
 import RedoIcon from './ic-redo.svg';
+import ReorderIcon from './ic-reorder.svg';
 import RestoreIcon from './ic-restore.svg';
 import RichTextIcon from './ic-text-rich.svg';
+import SaveIcon from './ic-save.svg';
+import SearchIcon from './ic-search.svg';
+import SearchIosIcon from './ic-search-ios.svg';
 import SecurityIcon from './ic-security.svg';
+import SelectAllIcon from './ic-select-all.svg';
+import SendIcon from './ic-send.svg';
 import ServerIcon from './ic-server.svg';
 import SettingsIcon from './ic-settings.svg';
+import ShareIcon from './ic-share.svg';
 import SignInIcon from './ic-signin.svg';
 import SignOutIcon from './ic-signout.svg';
-import SNLogoIcon from './ic-standard-notes.svg';
 import SNLogoAltIcon from './ic-standard-notes-2.svg';
 import SNLogoFull from './ic-sn-logo-full.svg';
+import SNLogoIcon from './ic-standard-notes.svg';
+import SortDescendingIcon from './ic-sort-descending.svg';
 import SpreadsheetsIcon from './ic-spreadsheets.svg';
+import StarFilledIcon from './ic-star-filled.svg';
 import StarIcon from './ic-star.svg';
+import StarVariantFilledIcon from './ic-star-variant-filled.svg';
 import SyncIcon from './ic-sync.svg';
 import TasksIcon from './ic-tasks.svg';
 import ThemesIcon from './ic-themes.svg';
 import TrashFilledIcon from './ic-trash-filled.svg';
 import TrashIcon from './ic-trash.svg';
+import TrashSweepFilledIcon from './ic-trash-sweep-filled.svg';
 import TrashSweepIcon from './ic-trash-sweep.svg';
 import TuneIcon from './ic-tune.svg';
 import UnarchiveIcon from './ic-unarchive.svg';
 import UndoIcon from './ic-undo.svg';
 import UnpinIcon from './ic-pin-off.svg';
+import UploadIcon from './ic-upload.svg';
 import UserAddIcon from './ic-user-add.svg';
 import UserIcon from './ic-user.svg';
 import UserSwitch from './ic-user-switch.svg';
@@ -95,23 +132,34 @@ export {
   AccessibilityIcon,
   AccountCircleIcon,
   AccountIllustration,
+  AddBoldIcon,
   AddIcon,
   ArchiveIcon,
   ArrowDownCheckmarkIcon,
-  ArrowUpIcon,
   ArrowDownIcon,
   ArrowLeftIcon,
   ArrowRightIcon,
+  ArrowsHorizontalIcon,
   ArrowsSortDownIcon,
   ArrowsSortUpIcon,
+  ArrowsVerticalIcon,
+  ArrowUpIcon,
+  AttachmentFileIcon,
   AuthenticatorIcon,
+  BackIosIcon,
   BlueDotIcon,
+  BoxFilledIcon,
+  BoxIcon,
+  CameraIcon,
+  CheckAllIcon,
   CheckBoldIcon,
-  CheckCircleIcon,
   CheckCircleFilledIcon,
+  CheckCircleIcon,
   CheckIcon,
   ChevronDownIcon,
+  ChevronLeftIcon,
   ChevronRightIcon,
+  ChevronUpIcon,
   CircleIcon,
   ClearCircleFilledIcon,
   CloseIcon,
@@ -122,63 +170,89 @@ export {
   DashboardIcon,
   DiamondIcon,
   DownloadIcon,
+  DrawIcon,
   EditorIcon,
   EmailIcon,
   EyeIcon,
   EyeOffIcon,
+  FileFilledIcon,
+  FileIcon,
+  FolderFilledIcon,
+  FolderIcon,
+  FolderKeyFilledIcon,
+  ForwardIosIcon,
   HashtagIcon,
   HashtagOffIcon,
+  HeartFilledIcon,
   HelpIcon,
   HistoryIcon,
   IlNotesIcon,
+  ImageIcon,
   InfoIcon,
   KeyboardIcon,
+  LinkIcon,
   LinkOffIcon,
   ListBulleted,
-  ListedIcon,
   ListedFilledIcon,
+  ListedIcon,
   LockFilledIcon,
   LockIcon,
   MarkdownIcon,
   MenuArrowDownAlt,
   MenuArrowDownIcon,
-  MenuArrowRight,
+  MenuArrowRightIcon,
   MenuCloseIcon,
+  MergeIcon,
   MoreIcon,
+  MoreVertIcon,
   NotesIcon,
   OpenInIcon,
   PasswordIcon,
-  PencilOffIcon,
-  PencilIcon,
   PencilFilledIcon,
+  PencilIcon,
+  PencilOffIcon,
   PinFilledIcon,
   PinIcon,
   PlainTextIcon,
+  PlusCircleFilledIcon,
+  PlusCircleIcon,
   PremiumFeatureIcon,
   PremiumIllustration,
   RedoIcon,
+  ReorderIcon,
   RestoreIcon,
   RichTextIcon,
+  SaveIcon,
+  SearchIcon,
+  SearchIosIcon,
   SecurityIcon,
+  SelectAllIcon,
+  SendIcon,
   ServerIcon,
   SettingsIcon,
+  ShareIcon,
   SignInIcon,
   SignOutIcon,
-  SNLogoIcon,
   SNLogoAltIcon,
   SNLogoFull,
+  SNLogoIcon,
+  SortDescendingIcon,
   SpreadsheetsIcon,
+  StarFilledIcon,
   StarIcon,
+  StarVariantFilledIcon,
   SyncIcon,
   TasksIcon,
   ThemesIcon,
   TrashFilledIcon,
   TrashIcon,
+  TrashSweepFilledIcon,
   TrashSweepIcon,
   TuneIcon,
   UnarchiveIcon,
   UndoIcon,
   UnpinIcon,
+  UploadIcon,
   UserAddIcon,
   UserIcon,
   UserSwitch,
