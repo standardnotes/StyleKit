de9d9357f3e31b9863196afa0c0bab6132f031df
Aman Harwara <amanharwara@protonmail.com>
2022-04-11 12:55:21 +0530

    feat: addTimedToast utility

---
+++ src/components/Toast/addTimedToast.ts
@@ -0,0 +1,34 @@
+import { addToast, dismissToast, updateToast } from './toastStore';
+import { Toast, ToastOptions } from './types';
+
+type InitialToastOptions = Omit<ToastOptions, 'message'> & {
+  message: (timeRemainingInSeconds: number) => string;
+};
+
+export const addTimedToast = (
+  initialOptions: InitialToastOptions,
+  callback: () => void,
+  timeInSeconds: number,
+): [Toast['id'], NodeJS.Timeout] => {
+  let timeRemainingInSeconds = timeInSeconds;
+
+  const intervalId = setInterval(() => {
+    if (timeRemainingInSeconds > 1) {
+      timeRemainingInSeconds--;
+      updateToast(toastId, {
+        message: initialOptions.message(timeRemainingInSeconds),
+      });
+    } else {
+      dismissToast(toastId);
+      clearInterval(intervalId);
+      callback();
+    }
+  }, 1000);
+
+  const toastId = addToast({
+    ...initialOptions,
+    message: initialOptions.message(timeRemainingInSeconds),
+  });
+
+  return [toastId, intervalId];
+};
